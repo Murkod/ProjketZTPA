9134db9966f6acc33669524de2b452f757663a01
Murkod <59425782+Murkod@users.noreply.github.com>
2025-01-23 21:32:07 +0100

    dsa

---
+++ Conversion Gateway/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Dodaj usługi do kontenera DI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Konfiguracja pipeline HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Endpointy
+app.MapGet("/", () => "Hello World!")
+   .WithName("GetHelloWorld")
+   .WithOpenApi();
+
+app.MapGet("/hello/{name}", (string name) => $"Hello, {name}!")
+   .WithName("GetHelloByName")
+   .WithOpenApi();
+
+app.Run();
